c2785c6346eeb2bf84db96d696de9a31625329c0
Peiman Matin <peyman_matin.2009@yahoo.com>
2023-04-16 13:24:18 +0430

    Build the arrow link component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
+import Link from "./components/Link";
+
 function App() {
   return (
     <div className="App">
-      <h1>heading 1</h1>
-      <h2>heading 2</h2>
-      <h3>heading 3</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero error
-        doloribus velit quis dolor ut, tenetur dolores vel placeat mollitia!
-      </p>
+      <Link />
     </div>
   );
 }
---
+++ src/components/Link.tsx
@@ -0,0 +1,12 @@
+import { BsArrowRight } from "react-icons/bs";
+
+const Link = () => {
+  return (
+    <a href="#" className="link-arrow">
+      Learn More
+      <BsArrowRight className="link__icon" />
+    </a>
+  );
+};
+
+export default Link;
