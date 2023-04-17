6ddd416e25e1344d76a87cc5e481d558071a9ab6
Peiman Matin <peyman_matin.2009@yahoo.com>
2023-04-17 12:19:21 +0430

    Implement the card component details.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,12 +2,20 @@ const Card = () => {
   return (
     <div className="card card--secondary">
       <header className="card__header">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam,
-        vero?
+        <h3 className="card__heading">Entry</h3>
+        <span className="card__price">$14</span>
+        <span className="card__date">/mount</span>
+        <span className="badge badge--secondary badge--small">10% Off</span>
+        <p className="card__text">Easy start on the cloud</p>
       </header>
       <div className="card__body">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam culpa
-        qui, cupiditate accusamus natus explicabo laborum repudiandae id
+        <ul className="list list--tick">
+          <li className="list__item">Unlimited Websites</li>
+          <li className="list__item">Unlimited Bandwidth</li>
+          <li className="list__item">100 GB SSD Storage</li>
+          <li className="list__item">3 GB RAM</li>
+        </ul>
+        <button className="btn btn--outline btn--block">buy now</button>
       </div>
     </div>
   );
