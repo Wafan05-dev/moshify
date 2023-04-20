28f4c71b98cd8e9907ffda87d1f54c887668cabe
Peiman Matin <peyman_matin.2009@yahoo.com>
2023-04-20 20:31:31 +0430

    Build the domain component.

---
+++ src/components/Domain.tsx
@@ -0,0 +1,30 @@
+import Search from "./Search";
+
+const Domain = () => {
+  return (
+    <section className="block container domain">
+      <header className="block__header">
+        <h2 className="domain__heading">
+          The best webhosting Starting at per month
+        </h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Voluptatibus, reprehenderit.
+        </p>
+      </header>
+      <Search />
+      <ul className="list domain__list">
+        <li>.com $9.26</li>
+        <li>
+          <span className="badge badge--secondary">.sg $7.91</span>
+        </li>
+        <li>.space $12.98</li>
+        <li>.info $14.15</li>
+        <li>.net $16.15</li>
+        <li>.xyz $10.15</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Domain;
