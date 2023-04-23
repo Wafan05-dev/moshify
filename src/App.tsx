99520875a2f070e5906acc7b8c6916f9ed8181fc
Peiman Matin <peyman_matin.2009@yahoo.com>
2023-04-23 18:19:21 +0430

    Fix the layout issue with the testimonial block on small screen devices.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Feature from "./components/Feature";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Plans from "./components/Plans";
+import Showcase from "./components/Showcase";
+import Testimonial from "./components/Testimonial";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Domain />
         <Plans />
         <Feature />
+        <Showcase />
+        <Testimonial />
       </main>
     </>
   );
